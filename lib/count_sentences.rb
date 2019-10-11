6e137294b895952cdb420fe9b8afa86fcbeaadba
Jacques Robert Plante <jonlypaffle@gmail.com>
2019-10-11 14:32:16 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,7 +15,7 @@ class String
   end
 
   def count_sentences
-    self.split(".","?","!").count
-    
+    split_sentences = self.split(".").count
+    split_sentences << self.split("!")
   end
 end
